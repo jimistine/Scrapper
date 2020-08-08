401bea2e78b1f9b7031cbfbd5cc3f72bf8ba0dab
jimistine <jrstine@me.com>
2020-08-07 20:22:15 -0400

    Added basic fuel consumption

--- Assets/Scripts/click_to_move.cs
+++ Assets/Scripts/click_to_move.cs
@@ -5,17 +5,20 @@ using UnityEngine;
 public class click_to_move : MonoBehaviour
 {
 
+public Vector3 startPos;
 public Vector3 pointClicked;
 
 public float speed;
+public float currentSpeed;
 public float startTime;
 public float journeyLength;
 public float fractionOfJourney;
+public float distanceToEnd;
 
 public float lerpTime = 1f;
 public float currentLerpTime;
 
-// public bool isMoving;
+public bool isMoving;
 
     // Start is called before the first frame update
     void Start()
@@ -26,30 +29,47 @@ public float currentLerpTime;
     // Update is called once per frame
     void Update()
     {
+        
         if (Input.GetMouseButtonDown(0)){
             //Debug.Log("clicked");
+            startPos = transform.position;
             startTime = Time.time;
-            //isMoving = true;
+            isMoving = true;
             Vector3 mousePos = Input.mousePosition;
             mousePos.z = Camera.main.nearClipPlane;
             pointClicked = Camera.main.ScreenToWorldPoint(mousePos);
-            Debug.Log("Point Clicked: " + pointClicked);
+            //Debug.Log("Point Clicked: " + pointClicked);
             journeyLength = Vector3.Distance(transform.position, pointClicked);
+
         }
 
-        //if(isMoving){
+    //Classic Lerp
+        float distCovered = (Time.time - startTime) * speed;
+        fractionOfJourney = distCovered / journeyLength;
+        transform.position = Vector3.Lerp(transform.position, pointClicked, fractionOfJourney);
+
+        currentSpeed = Vector3.Distance(startPos, transform.position) / (Time.time - startTime);
+        
+        
+
+
+        distanceToEnd = Vector3.Distance(transform.position, pointClicked);
+        if(distanceToEnd < .1f){
+            // Debug.Log(Vector3.Distance(transform.position, pointClicked));
+            isMoving = false;
+        }
             
     //Easeing
-            if (Input.GetMouseButtonDown(0)) {
-                currentLerpTime = 0f;
-            }
+            // if (Input.GetMouseButtonDown(0)) {
+            //     currentLerpTime = 0f;
+            // }
  
-            //increment timer once per frame
-            currentLerpTime += Time.deltaTime;
-            if (currentLerpTime > lerpTime) {
-                currentLerpTime = lerpTime;
-            }
-            float t = currentLerpTime / lerpTime;
+            // //increment timer once per frame
+            // currentLerpTime += Time.deltaTime;
+            // if (currentLerpTime > lerpTime) {
+            //     currentLerpTime = lerpTime;
+            // }
+            // float t = currentLerpTime / lerpTime;
 
         // Ease out
             //t = Mathf.Sin(t * Mathf.PI * speed);
@@ -62,17 +82,6 @@ public float currentLerpTime;
     // Constant Speed
             //transform.position = Vector3.MoveTowards(transform.position, pointClicked, Time.deltaTime * speed); 
 
-    //Classic Lerp
-            float distCovered = (Time.time - startTime) * speed;
-            fractionOfJourney = distCovered / journeyLength;
-            transform.position = Vector3.Lerp(transform.position, pointClicked, fractionOfJourney);
-
-        
-            if(Vector3.Distance(transform.position, pointClicked) < .1f){
-                Debug.Log(Vector3.Distance(transform.position, pointClicked));
-               // isMoving = false;
-                
-            }
-        //}
+    
     }
 }
---
+++ Assets/Scripts/fuel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fuel : MonoBehaviour
+{
+
+public click_to_move click_To_Move;
+
+public float currentSpeed;
+public float currentFuelLevel;
+public float fuelEfficiency;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        currentSpeed = click_To_Move.currentSpeed;
+        float fuelLossRate = fuelEfficiency * currentSpeed;
+
+        if(click_To_Move.isMoving && float.IsNaN(fuelLossRate) == false){
+            currentFuelLevel -= fuelLossRate * .01f;
+        }
+    }
+}
